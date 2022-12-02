1d06af043f99bdcfbf79417114592b24ea62e138
Daniel Marmelo <daniel.marmelo@hotmail.com>
2022-12-02 16:10:02 +0000

    Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,37 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun List<String>.elfInventories(): List<List<Int>> {
+        var rest = this
+        return buildList {
+            while (rest.isNotEmpty()) {
+                val elfInventory = rest.takeWhile { it.isNotBlank() }.map { it.toInt() }
+                add(elfInventory)
+                rest =
+                    if (elfInventory.size == rest.size) emptyList()
+                    else rest.subList(elfInventory.size + 1, rest.size)
+            }
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(elfInventories: List<List<Int>>): Int {
+        return elfInventories.maxOf { it.sum() }
+    }
+
+    fun part2(elfInventories: List<List<Int>>): Int {
+        return elfInventories
+            .map { it.sum() }
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInputElfInventories = testInput.elfInventories()
+    check(part1(testInputElfInventories) == 24000)
+    check(part2(testInputElfInventories) == 45000)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val inputElfInventories = input.elfInventories()
+    println(part1(inputElfInventories))
+    println(part2(inputElfInventories))
 }
