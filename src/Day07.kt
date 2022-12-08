916d9864cfb931c972aa8f2375cc209eed9f7bc7
Daniel Marmelo <daniel.marmelo@hotmail.com>
2022-12-08 23:20:07 +0000

    Day 7 solution refactor

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,136 +1,121 @@
-import java.nio.file.Path
-import kotlin.io.path.Path
-import kotlin.io.path.div
-import kotlin.io.path.name
-import kotlin.math.abs
+private typealias Command = Pair<String, List<String>>
 
-typealias Command = Pair<String, List<String>>
-
-private interface FsEntry {
+sealed class FsEntry(
     val name: String
+) {
+    abstract val size: Int
 }
 
-private data class Directory(
-    override val name: String,
+private class Directory(
+    name: String,
     var children: List<FsEntry> = emptyList()
-) : FsEntry
+) : FsEntry(name) {
+    override val size: Int
+        get() = children.sumOf { it.size }
+}
 
-private data class File(
-    override val name: String,
-    val size: Int
-) : FsEntry
+private class File(
+    name: String,
+    override val size: Int,
+) : FsEntry(name)
+
+private fun Directory.findAllSubDirectorires(): List<Directory> =
+    children
+        .filterIsInstance<Directory>()
+        .fold(emptyList()) { acc, directory ->
+            acc + directory + directory.findAllSubDirectorires()
+        }
+
+private fun Directory.findSubDirectory(path: String): Directory? {
+    val subPaths = path.split("/")
+        .dropWhile { it == "" }
+        .dropLastWhile { it == "" }
+
+    var currentDirectory = this
+    for (subPath in subPaths) {
+        currentDirectory = currentDirectory.children
+            .filterIsInstance<Directory>()
+            .find { it.name == subPath } ?: return null
+    }
+    return currentDirectory
+}
 
 fun main() {
     fun String.parseInput(): List<Command> {
         return split("$ ")
             .drop(1)
+            .map { it.trim() }
             .map {
                 val lines = it.lines()
-                lines.first() to lines.drop(1).dropLastWhile { line -> line == "" }
+                lines.first() to lines.drop(1)
             }
-    }
-
-    fun findDir(currentPath: Path, root: Directory): Directory {
-        var currentDir = root
-        for (path in currentPath) {
-            currentDir = currentDir.children
-                .filterIsInstance<Directory>()
-                .find { it.name == path.name } as Directory
-        }
-        return currentDir
     }
 
     fun List<Command>.buildFileTree(): Directory {
-        var currentPath = Path("/")
-        val fileTree = Directory("/")
+        var currentPath = "/"
+        val root = Directory("/")
 
         forEach { (command, output) ->
-            if (command.startsWith("cd")) {
-                val newDir = command.split(" ").last()
-
-                /*val currentDir = findDir(currentPath, fileTree)
-                currentDir.children += Directory(newDir)*/
-
-                currentPath /= newDir
-                currentPath = currentPath.normalize()
-            } else if (command == "ls") {
-                val currentDir = findDir(currentPath, fileTree)
-
-                val newEntries = output.map {
-                    val (sizeOrType, name) = it.split(" ")
-                    if (sizeOrType == "dir") {
-                        Directory(name)
-                    } else {
-                        File(name, sizeOrType.toInt())
+            with(command) {
+                when {
+                    startsWith("cd") -> {
+                        currentPath = when (val argument = command.removePrefix("cd ")) {
+                            "/" -> "/"
+                            ".." -> currentPath.substringBeforeLast("/", "/")
+                            else ->
+                                if (currentPath.endsWith("/")) currentPath + argument
+                                else "$currentPath/$argument"
+                        }
                     }
-                }
 
-                currentDir.children += newEntries
-            } else {
-                error("Unknown comand $command")
+                    startsWith("ls") -> {
+                        val newEntries = output.map {
+                            val (sizeOrType, name) = it.split(" ")
+                            if (sizeOrType == "dir") {
+                                Directory(name)
+                            } else {
+                                File(name, sizeOrType.toInt())
+                            }
+                        }
+
+                        root.findSubDirectory(currentPath)?.apply {
+                            children += newEntries
+                        } ?: error("Unknown path $currentPath")
+                    }
+
+                    else -> error("Unknown command $command")
+                }
             }
         }
-        return fileTree
+        return root
     }
 
-    fun calcDirectorySize(directory: Directory): Int {
-        val filesSize = directory.children
-            .filterIsInstance<File>()
-            .sumOf { it.size }
-
-        val directoriesSize = directory.children
-            .filterIsInstance<Directory>()
-            .sumOf { calcDirectorySize(it) }
-
-        return filesSize + directoriesSize
+    fun part1(root: Directory): Int {
+        return root.findAllSubDirectorires()
+            .map { it.size }
+            .filter { it <= 100_000 }
+            .sum()
     }
 
-    fun flattenTree(root: Directory): List<Directory> {
-        return root.children
-            .filterIsInstance<Directory>()
-            .fold(emptyList()) { acc, directory ->
-                acc + directory + flattenTree(directory)
-            }
-    }
-
-
-    fun part1(input: List<Command>): Int {
-        val fileTree = input.buildFileTree()
-
-        val flattenTree = flattenTree(fileTree)
-        val directorySize = flattenTree
-            .map { it to calcDirectorySize(it) }
-
-        return directorySize
-            .filter { it.second <= 100_000 }
-            .sumOf { it.second }
-    }
-
-    fun part2(input: List<Command>): Int {
-        val fileTree = input.buildFileTree()
-
+    fun part2(root: Directory): Int {
         val totalDiskSpace = 70_000_000
         val spaceRequired = 30_000_000
-        val spaceUsed = calcDirectorySize(fileTree)
+        val spaceUsed = root.size
         val unusedSpace = totalDiskSpace - spaceUsed
-        val spaceToClean = abs(unusedSpace - spaceRequired)
+        val spaceToClean = spaceRequired - unusedSpace
 
-
-        val flattenTree = flattenTree(fileTree)
-        val directorySize = flattenTree
-            .map { it to calcDirectorySize(it) }
-
-        return directorySize
-            .filter { it.second >= spaceToClean }
-            .minOf { it.second }
+        return root.findAllSubDirectorires()
+            .map { it.size }
+            .filter { it >= spaceToClean }
+            .min()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInputText("Day07_test").parseInput()
+    val testInput = readInputText("Day07_test").parseInput().buildFileTree()
     check(part1(testInput) == 95_437)
     check(part2(testInput) == 24_933_642)
 
-    val input = readInputText("Day07").parseInput()
+    val input = readInputText("Day07").parseInput().buildFileTree()
     println(part1(input))
     println(part2(input))
 }
