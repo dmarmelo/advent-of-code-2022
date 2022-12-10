a599e61ee88d2b21247fd39100ddc6292be48cec
Daniel Marmelo <daniel.marmelo@hotmail.com>
2022-12-10 20:30:01 +0000

    Day 10 solution refactor

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,39 +1,50 @@
-private typealias Instruction = Pair<String, Int?>
+private sealed class Instruction {
+    abstract val cycles: Int
+
+    object Noop : Instruction() {
+        override val cycles: Int
+            get() = 1
+    }
+
+    data class Addx(val argument: Int) : Instruction() {
+        override val cycles: Int
+            get() = 2
+    }
+}
 
 fun main() {
-    fun List<String>.parseInput(): List<Instruction> = map {
-        if (it.contains(" ")) {
-            it.split(" ").let { (instruction, argument) ->
-                instruction to argument.toInt()
-            }
-        } else {
-            it to null
+    fun String.toInstruction() = with(this) {
+        when {
+            equals("noop") -> Instruction.Noop
+            startsWith("addx") -> Instruction.Addx(removePrefix("addx ").toInt())
+            else -> error("Unknown instriction $this")
         }
     }
 
+    fun List<String>.parseInput(): List<Instruction> = map { it.toInstruction() }
+
     fun List<Instruction>.run(onCycle: (cycle: Int, x: Int) -> Unit) {
         val iterator = iterator()
-        var line: Instruction? = null
+
+        // Load first Instruction
+        var instruction = iterator.next()
+        var remainigCycles = instruction.cycles
+
         var x = 1
-        var instructionCycles = 0
-        for (clock in (1..240)) {
-            if (instructionCycles == 0) {
-                line = iterator.next()
-                val (instruction, _) = line
-                when (instruction) {
-                    "noop" -> instructionCycles = 1
-                    "addx" -> instructionCycles = 2
+
+        for (cycle in (1..240)) {
+            onCycle(cycle, x)
+            remainigCycles--
+            if (remainigCycles == 0) {
+                // Execute Instruction
+                if (instruction is Instruction.Addx) {
+                    x += instruction.argument
                 }
-            }
 
-            onCycle(clock, x)
-
-            instructionCycles--
-
-            if (instructionCycles == 0 && line != null) {
-                val (instruction, argument) = line
-                if (instruction == "addx") {
-                    x += argument!!
+                // Load next Instruction if there is one
+                if (iterator.hasNext()) {
+                    instruction = iterator.next()
+                    remainigCycles = instruction.cycles
                 }
             }
         }
@@ -51,16 +62,12 @@ fun main() {
 
     fun part2(input: List<Instruction>): String {
         val crtWidth = 40
-        val crt = ""
+        var crt = ""
         input.run { cycle, x ->
             val crtHorizontalPosition = (cycle - 1) % crtWidth
-            print(
-                if (crtHorizontalPosition in (x - 1..x + 1)) '#'
-                else '.'
-            )
-            if (crtHorizontalPosition == crtWidth - 1) println()
+            crt += if (crtHorizontalPosition in (x - 1..x + 1)) "#" else "."
+            if (crtHorizontalPosition == crtWidth - 1) crt += "\n"
         }
-
         return crt
     }
 
